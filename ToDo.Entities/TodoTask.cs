eeaf0fcb354afa18d422c0245c81e32e1d1de3aa
Joel Parkinson <joelparkinson@live.co.uk>
2019-09-08 01:14:24 +0100

    Update the TodoTask entity
    Extended ready for further feature implementation.

--- ToDo.Entities/TodoTask.cs
+++ ToDo.Entities/TodoTask.cs
@@ -5,6 +5,9 @@ namespace ToDo.Entities
     public class TodoTask : IUnique
     {
         public Guid Id { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
+        public DateTime CompletionDate { get; set; }
     }
 }
