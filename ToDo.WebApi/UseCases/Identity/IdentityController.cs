e4b12c6fb3b04cf9201f056adad87e1bfbd950cc
Laxynium <grzesiu15432@gmail.com>
2019-10-05 06:34:24 -0700

    Add identity verification endpoint for ensuring that credentials are still valid.

---
+++ ToDo.WebApi/UseCases/Identity/IdentityController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo.WebApi.UseCases.Identity
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/identity")]
+    public class IdentityController : Controller
+    {
+        [HttpGet("verify")]
+        public IActionResult Verify()
+        {
+            return Ok("OK");
+        }
+    }
+}
