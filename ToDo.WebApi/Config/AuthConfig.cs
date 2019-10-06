53b9a359a6c177c12251c6b361a234ce7d2ead80
flyznex <thuanpt.dev@gmail.com>
2019-10-06 12:42:33 +0700

    clean code syntax

--- ToDo.WebApi/Config/AuthConfig.cs
+++ ToDo.WebApi/Config/AuthConfig.cs
@@ -5,7 +5,5 @@ namespace ToDo.WebApi.Config {
         public static string SecretKey  = "TuqBF-Gc4sC58oHm8EaKCQCTqsxH1SZ6AVUsfBuQE0U";
         public static string SecurityAlgorithm  = SecurityAlgorithms.HmacSha256Signature;
         public static int ExpireSeconds = 86400; //1 day
-
     }
-    
 }
